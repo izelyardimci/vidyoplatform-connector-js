7f3c4c7d462e0284ff33a0e1aa18a4c91439c321
Ekin YILDIRIM <ekinyi@netas.com.tr>
2021-03-08 21:52:05 +0300

    txt file options

--- VidyoConnector.js
+++ VidyoConnector.js
@@ -63,6 +63,39 @@ function StartVidyoConnector(VC, useTranscodingWebRTC, performMonitorShare, webr
             );
           },
         });
+////////////////////////////////////////
+        function saveTextAsFile() {
+            var textToWrite = document.getElementById('chatmessage').innerHTML;
+            var textFileAsBlob = new Blob([ textToWrite ], { type: 'text/plain' });
+            var fileNameToSaveAs = "file.txt"; //filename.extension
+          
+            var downloadLink = document.createElement("a");
+            downloadLink.download = fileNameToSaveAs;
+            downloadLink.innerHTML = "Download File";
+            if (window.webkitURL != null) {
+              // Chrome allows the link to be clicked without actually adding it to the DOM.
+              downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+            } else {
+              // Firefox requires the link to be added to the DOM before it can be clicked.
+              downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+              downloadLink.onclick = destroyClickedElement;
+              downloadLink.style.display = "none";
+              document.body.appendChild(downloadLink);
+            }
+          
+            downloadLink.click();
+          }
+          
+          var button = document.getElementById('save');
+          button.addEventListener('click', saveTextAsFile);
+          
+          function destroyClickedElement(event) {
+            // remove the link from the DOM
+            document.body.removeChild(event.target);
+          }
+
+/////////////////////////////////////////////
+
 
         // Populate the connectionStatus with the client version
         vidyoConnector.GetVersion().then(function(version) {
