fb04a0d79a4a417a9d274ae18243f267b7dda0ce
Izel YARDIMCI <izely@netas.com.tr>
2021-07-13 11:20:37 +0300

    change language to turkish

--- VidyoConnector.js
+++ VidyoConnector.js
@@ -41,7 +41,7 @@ function StartVidyoConnector(VC, useTranscodingWebRTC, performMonitorShare, webr
                 }
             recognition.continuous = true;
             recognition.interimResults = true;
-            recognition.lang = "en-US"
+            recognition.lang = "tr-TR"
             recognition.onresult = function (event) {
                 for (var i = event.resultIndex; i < event.results.length; ++i) {
                     if (event.results[i].isFinal) {
